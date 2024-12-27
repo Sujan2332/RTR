728fde9171dde92d31390110cf6b960a16e45c3a
Sujan2332 <saisujan.s03@gmail.com>
2024-12-27 13:14:58 +0530

    FIRST

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reverse-traffic-racer/', 
+  base: '/RTR/', 
   build: {
     outDir: 'dist',  // Make sure this is where you want to build your files
   },
